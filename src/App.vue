fe7d171661ec6196a0262c3ec7cfdee20b0b2bef
Hyohwak Lee <crewe1746@naver.com>
2021-11-25 00:05:25 +0900

    Removed app-bar and router

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,7 @@
 <template>
-  <v-app id="customFont">
-    <v-app-bar app color="yellow">
-      <v-toolbar-title class="font-weight-bold">Sketch It</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="black"
-            v-bind="attrs"
-            v-on="on"
-            icon
-            x-large
-            id="help"
-            @click="openGuide()"
-          >
-            <v-icon>mdi-help-box</v-icon>
-          </v-btn>
-        </template>
-        <span>Quick Guide</span>
-      </v-tooltip>
-      <v-divider vertical></v-divider>
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="black" v-bind="attrs" v-on="on" icon x-large id="save">
-            <v-icon>mdi-content-save</v-icon>
-          </v-btn>
-        </template>
-        <span>Save Sketch</span>
-      </v-tooltip>
-      <v-divider vertical></v-divider>
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="black" v-bind="attrs" v-on="on" icon x-large id="clear">
-            <v-icon>mdi-eraser</v-icon>
-          </v-btn>
-        </template>
-        <span>Clear Canvas</span>
-      </v-tooltip>
-    </v-app-bar>
-    <v-main>
-      <v-overlay :value="guide">
+  <v-app id="app">
+    <v-main app>
+      <v-overlay :value="guide" opacity="0.8">
         <v-stepper v-model="pagination" vertical>
           <v-stepper-step :complete="pagination > 1" step="1">
             Select The Model!
@@ -100,20 +62,230 @@
           </v-stepper-content>
         </v-stepper>
       </v-overlay>
-      <router-view />
+      <v-container fluid>
+        <v-row>
+          <v-col xs="8" sm="4">
+            <v-select
+              :items="modelList"
+              v-model="currentModel"
+              label="Select Model"
+              @change="modelChange"
+              height="30px"
+            ></v-select>
+          </v-col>
+          <v-col xs="4" sm="4">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  class="hidden-xs-only"
+                  color="black"
+                  v-bind="attrs"
+                  v-on="on"
+                  icon
+                  x-large
+                  id="help"
+                  @click="openGuide()"
+                >
+                  <v-icon>mdi-help-box</v-icon>
+                </v-btn>
+              </template>
+              <span>Quick Guide</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="black"
+                  v-bind="attrs"
+                  v-on="on"
+                  icon
+                  x-large
+                  id="save"
+                >
+                  <v-icon>mdi-content-save</v-icon>
+                </v-btn>
+              </template>
+              <span>Save Sketch</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="black"
+                  v-bind="attrs"
+                  v-on="on"
+                  icon
+                  x-large
+                  id="clear"
+                >
+                  <v-icon>mdi-trash-can-outline</v-icon>
+                </v-btn>
+              </template>
+              <span>Clear Canvas</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-chip-group column class="hidden-xs-only">
+              <v-chip
+                v-for="chip in chips"
+                :key="chip"
+                close
+                @click:close="chips.splice(chips.indexOf(chip), 1)"
+                >{{ chip }}</v-chip
+              >
+            </v-chip-group>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-container fluid>
+        <v-row>
+          <v-col id="canvasContainer">
+            <div id="canvas"></div>
+          </v-col>
+        </v-row>
+        <v-snackbar
+          color="primary"
+          v-model="isLoaded"
+          timeout="2000"
+          dark
+          shaped
+          >sketchRNN Model Loaded!
+        </v-snackbar>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 <script>
+import ml5 from "ml5";
+import P5 from "p5";
+import modelList from "@/assets/models";
+
+let count = Math.floor(Math.random() * 100);
+
 export default {
   name: "App",
   data() {
     return {
+      model: null,
+      previousPen: "down",
+      x: null,
+      y: null,
+      strokePath: null,
+      seedStrokes: [],
+      canvas: null,
+      modelList: modelList,
+      currentModel: "cat",
+      isLoaded: false,
+      chips: [],
       guide: false,
       pagination: 1,
     };
   },
+  mounted() {
+    const script = (p) => {
+      p.setup = () => {
+        const canvasSize = document
+          .getElementById("canvasContainer")
+          .getBoundingClientRect();
+        const canvasWidth = Math.floor(canvasSize.width);
+        const canvasHeight = Math.floor(canvasSize.height);
+        this.canvas = p.createCanvas(canvasWidth, canvasHeight);
+        this.canvas.mousePressed(p.resetDrawing);
+        this.canvas.mouseReleased(p.startSketchRNN);
+        p.background(230);
+        this.modelChange();
+
+        const saveBtn = p.select("#save");
+        saveBtn.mousePressed(p.saveDrawing);
+        const clearBtn = p.select("#clear");
+        clearBtn.mousePressed(p.clearDrawing);
+      };
+
+      p.draw = () => {
+        if (p.mouseIsPressed) {
+          p.stroke(0, 0, 255);
+          p.strokeWeight(4);
+          p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
+
+          const userStroke = {
+            dx: p.mouseX - p.pmouseX,
+            dy: p.mouseY - p.pmouseY,
+            pen: "down",
+          };
+          this.seedStrokes.push(userStroke);
+        }
+
+        if (this.strokePath) {
+          if (this.previousPen === "down") {
+            p.stroke(0);
+            p.strokeWeight(4);
+            p.line(
+              this.x,
+              this.y,
+              this.x + this.strokePath.dx,
+              this.y + this.strokePath.dy
+            );
+          }
+          this.x += this.strokePath.dx;
+          this.y += this.strokePath.dy;
+          this.previousPen = this.strokePath.pen;
+
+          if (this.strokePath.pen !== "end") {
+            this.strokePath = null;
+            this.model.generate(p.gotStroke);
+          }
+        }
+      };
+
+      p.resetDrawing = () => {
+        this.seedStrokes = [];
+        this.model.reset();
+      };
+
+      p.startSketchRNN = () => {
+        this.x = p.mouseX;
+        this.y = p.mouseY;
+        this.model.generate(this.seedStrokes, p.gotStroke);
+      };
+
+      p.gotStroke = (err, result) => {
+        this.strokePath = result;
+      };
+
+      p.saveDrawing = () => {
+        p.saveCanvas(this.canvas, "sketch" + count, "png");
+        count++;
+      };
+
+      p.clearDrawing = () => {
+        p.clear();
+        p.background(234);
+        this.chips = [this.currentModel];
+      };
+
+      p.windowResized = () => {
+        const resizedSize = document
+          .getElementById("canvasContainer")
+          .getBoundingClientRect();
+        const resizedWidth = Math.floor(resizedSize.width);
+        const resizedHeight = Math.floor(resizedSize.height);
+        p.resizeCanvas(resizedWidth, resizedHeight);
+        p.background(230);
+      };
+    };
+    new P5(script, "canvas");
+  },
   methods: {
+    modelReady: function () {
+      this.isLoaded = true;
+    },
+    modelChange: function () {
+      this.model = ml5.sketchRNN(this.currentModel, this.modelReady);
+      this.chips.push(this.currentModel);
+      if (this.chips.length > 8) {
+        this.chips.shift();
+      }
+    },
     openGuide: function () {
       this.guide = true;
     },
@@ -121,15 +293,5 @@ export default {
 };
 </script>
 <style>
-@font-face {
-  font-family: "CookieRun-Regular";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/CookieRun-Regular.woff")
-    format("woff");
-  font-weight: normal;
-  font-style: normal;
-}
-
-#customFont {
-  font-family: "CookieRun-Regular";
-}
+@import "./assets/index.css";
 </style>
