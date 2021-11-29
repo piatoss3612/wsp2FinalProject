160cfab12afee7d13c61da417785b1827bb708c3
Hyohwak Lee <crewe1746@naver.com>
2021-11-29 16:37:20 +0900

    Added some comments

--- src/App.vue
+++ src/App.vue
@@ -41,12 +41,7 @@
             <small>click the button</small>
           </v-stepper-step>
           <v-stepper-content step="4">
-            <v-card
-              color="grey lighten-1"
-              class="mb-12"
-              height="80px"
-              width="360px"
-            >
+            <v-card color="white" class="mb-12" height="80px" width="360px">
               <v-img src="./assets/guide/step4.png" position="center"></v-img>
             </v-card>
             <v-btn
@@ -162,13 +157,12 @@
       <v-dialog v-model="saveDialogIsOpen" eager>
         <v-card outlined>
           <v-card-title>Save Sketch</v-card-title>
-          <v-form ref="form" class="ma-2 pa-2" lazy-validation>
+          <v-form ref="form" class="ma-2 pa-2">
             <v-text-field
               v-model="saveTitle"
               :counter="10"
               label="Title"
               :rules="titleRules"
-              required
               outlined
             ></v-text-field>
             <v-select
@@ -176,16 +170,14 @@
               :items="fileTypes"
               label="File Type"
               :rules="[(v) => !!v || 'You must choose type']"
-              required
               outlined
             ></v-select>
-            <v-btn class="ma-2" color="success" id="save"> Save </v-btn>
+            <v-btn class="ma-2" color="success" id="save">Save</v-btn>
             <v-btn
               class="ma-2"
               dark
               @click="saveDialogIsOpen = !saveDialogIsOpen"
-            >
-              Close
+              >Close
             </v-btn>
           </v-form>
         </v-card>
@@ -194,25 +186,28 @@
   </v-app>
 </template>
 <script>
+// import ml5 and p5 libraries
 import ml5 from "ml5";
 import P5 from "p5";
+// import supported models list
 import modelList from "@/assets/models";
 
 export default {
   name: "App",
   data() {
     return {
-      model: null,
-      modelList: modelList,
-      currentModel: "cat",
-      modelIsLoaded: false,
-      chips: [],
-      modelSelectIsOpen: false,
-      guideIsOpen: false,
-      saveDialogIsOpen: false,
-      pagination: 1,
+      model: null, // initialize ml5.sketchRNN instance here
+      modelList: modelList, // array of models that ml5 supports
+      currentModel: "cat", // parameter of instance initialization
+      modelIsLoaded: false, // change snackbar visibility
+      modelSelectIsOpen: false, // whether v-select list is opened or not
+      guideIsOpen: false, // change v-overlay visibility
+      saveDialogIsOpen: false, // change v-dialog visibility
+      chips: [], // array of currentModel
+      pagination: 1, // v-stepper's step
       saveTitle: "sketch",
       titleRules: [
+        // check validation of saveTitle
         (v) => !!v || "Title is required",
         (v) => (v && v.length <= 10) || "Title must be less than 10 characters",
       ],
@@ -221,60 +216,88 @@ export default {
     };
   },
   mounted() {
-    let x;
-    let y;
+    let x, y; // current location of drawing
     let previousPen = "down";
+    // current "stroke" of the drawing
     let strokePath;
     let seedStrokes = [];
+    // storing a reference to the canvas
     let canvas;
+    // script is seed to generate p5 instance
+    // argument p is p5 object and it is used to refer p5 API
     const script = (p) => {
+      // setup() and draw() function are kind of constructor of p5 instance
+      // they should be defined attached to p (object)
       p.setup = () => {
+        // get the size of element(id="canvas") and its position relative to the viewport
         const canvasSize = document
           .getElementById("canvas")
           .getBoundingClientRect();
+        // get width from canvasSize
         const canvasWidth = Math.floor(canvasSize.width);
+        // get height from canvasSize
         const canvasHeight = Math.floor(canvasSize.height);
+        // create canvas, this can be called only at initialization of setup()
         canvas = p.createCanvas(canvasWidth, canvasHeight);
+        // set event listener: if mouse is pressed on canvas, call resetDrawing
         canvas.mousePressed(resetDrawing);
+        // set event listener: if mouse is released, call startSketchRNN
         canvas.mouseReleased(startSketchRNN);
-        p.background(255); // white
-        this.loadModel();
+        p.background(255); // set background color to white
+        this.loadModel(); // initialize sketchRNN model
 
+        // find element with id="save"
         const saveBtn = p.select("#save");
+        // add event listener: if mouse is pressed on saveBtn, call saveDrawing
         saveBtn.mousePressed(saveDrawing);
+        // find element with id="clear"
         const clearBtn = p.select("#clear");
+        // add event listener: if mouse is pressed on clearBtn, call clearDrawing
         clearBtn.mousePressed(clearDrawing);
       };
 
       p.draw = () => {
+        // this part is for user to draw
+        // in the case of v-overlay & v-dialog & v-select is not opened
+        // and mouse is pressed on canvas
         if (
           !this.guideIsOpen &&
           !this.saveDialogIsOpen &&
           !this.modelSelectIsOpen &&
           p.mouseIsPressed
         ) {
+          // color of line is blue
           p.stroke(0, 0, 255);
+          // line weight is 4
           p.strokeWeight(4);
+          // draw along the line drawn by the user
           p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
 
+          // create a "stroke path" with dx, dy, and pen
           const userStroke = {
             dx: p.mouseX - p.pmouseX,
             dy: p.mouseY - p.pmouseY,
             pen: "down",
           };
+          // Add to the array
           seedStrokes.push(userStroke);
         }
 
+        // this part is for AI to draw
+        // if something new to draw
         if (strokePath) {
+          // if the pen is down, draw a line
           if (previousPen === "down") {
             p.stroke(0);
             p.strokeWeight(4);
             p.line(x, y, x + strokePath.dx, y + strokePath.dy);
           }
+          // move the pen
           x += strokePath.dx;
           y += strokePath.dy;
           previousPen = strokePath.pen;
 
+          // if the drawing is complete
           if (strokePath.pen !== "end") {
             strokePath = null;
             this.model.generate(gotStroke);
@@ -282,38 +305,48 @@ export default {
         }
       };
 
+      // this makes canvas responsive on breakpoint
       p.windowResized = () => {
+        // if v-dialog and v-guide is not opened, handle the resize event
         if (!this.saveDialogIsOpen && !this.guideIsOpen) {
           const resizedSize = document
             .getElementById("canvas")
             .getBoundingClientRect();
           const resizedWidth = Math.floor(resizedSize.width);
           const resizedHeight = Math.floor(resizedSize.height);
+          // resize the canvas
           p.resizeCanvas(resizedWidth, resizedHeight);
           p.background(255);
         }
       };
 
+      // reset the drawing
       const resetDrawing = () => {
-        seedStrokes = [];
-        this.model.reset();
+        seedStrokes = []; // clear seed strokes
+        this.model.reset(); // reset model
       };
 
+      // sketchRNN takes over
       const startSketchRNN = () => {
+        // start where the mouse left off
         x = p.mouseX;
         y = p.mouseY;
+        // generate with the seedStrokes
         this.model.generate(seedStrokes, gotStroke);
       };
 
+      // get a new stroke path
       const gotStroke = (err, result) => {
         strokePath = result;
       };
 
       const saveDrawing = () => {
+        // if validation of form is right, save the sketch
         if (this.$refs.form.validate()) {
           p.saveCanvas(canvas, this.saveTitle, this.saveType);
           this.saveTitle = "sketch";
           this.saveType = null;
+          // close v-dialog
           this.saveDialogIsOpen = false;
         }
       };
@@ -324,22 +357,29 @@ export default {
         this.chips = [this.currentModel];
       };
     };
+    // create p5 instance then render it on element that has id "canvas"
     new P5(script, "canvas");
   },
   methods: {
     modelReady: function () {
+      // if model is ready, display v-snackbar
       this.modelIsLoaded = true;
     },
     loadModel: function () {
+      // initialize sketchRNN instance
+      // then if model is ready, trigger modelReady function
       this.model = ml5.sketchRNN(this.currentModel, this.modelReady);
+      // if currentModel is not in array of chips, push it in the array
       if (!this.chips.includes(this.currentModel)) {
         this.chips.push(this.currentModel);
       }
     },
     openGuide: function () {
+      // change visibility of v-overlay
       this.guideIsOpen = true;
     },
     openDialog: function () {
+      // change visibility of v-dialog
       this.saveDialogIsOpen = true;
     },
   },
